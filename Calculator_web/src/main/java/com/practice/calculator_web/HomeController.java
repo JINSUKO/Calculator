112fed180d32b62dd120893685d0802f4ff338ea
고진수 <rhwkwk789@gmail.com>
2024-06-03 22:19:27 +0900

    set spring initial configures

---
+++ Calculator_web/src/main/java/com/practice/calculator_web/HomeController.java
@@ -0,0 +1,17 @@
+package com.practice.calculator_web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Object message() {
+
+        return new LinkedHashMap<>();
+    }
+}
