2e6b037faa729013a3f0ecd35d2e3e8b2cf757c8
고진수 <rhwkwk789@gmail.com>
2024-06-09 00:21:39 +0900

    html: change desgin add more buttons
    - add more buttons
    - add delete.svg
    - add get delete text feature

--- Calculator_web/src/main/resources/static/calculator.js
+++ Calculator_web/src/main/resources/static/calculator.js
@@ -12,8 +12,15 @@ function load_calculator_page() {
 
 function click_buttons() {
     let buttons_parent = document.querySelectorAll('.calculator > .arithmetic-buttons');
-
+    let input_display = "";
+    let mathemeatical_expression = "";
     buttons_parent[0].addEventListener("click", (e) => {
-       console.log(e.target.innerText);
+        console.log(e.target.innerText);
+        if (e.target.innerText == "") {
+            console.log(e.target.getAttribute('alt'));
+        }
+
+
+
     });
 }
