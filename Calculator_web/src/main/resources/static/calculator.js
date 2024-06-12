602f2e0e6516d705dcdfb52254b672035c0f6fe3
고진수 <rhwkwk789@gmail.com>
2024-06-12 13:23:05 +0900

    feature: input validation
    - assign input value in 'mathemeatical_expression' var
    - modify validation codes
    - seperate arithmetic operators and period charactor for input validation
    - add more input validation of the period character

--- Calculator_web/src/main/resources/static/calculator.js
+++ Calculator_web/src/main/resources/static/calculator.js
@@ -13,8 +13,10 @@ function load_calculator_page() {
 function click_buttons() {
     const buttons_parent = document.querySelectorAll('.calculator > .arithmetic-buttons');
     let input_display = document.querySelector(`.calculator > .display-container > .display`);
-    let mathemeatical_expression = "";
+    let mathemeatical_expression = input_display.value;
 
+
+    console.dir(input_display);
     console.log(input_display);
 
     const nums = [`0`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`];
@@ -31,9 +33,9 @@ function click_buttons() {
         console.log(value);
 
         let tmp = "";
+        let char_last = mathemeatical_expression.substring(mathemeatical_expression.length - 1, mathemeatical_expression.length);
 
         if (isNaN(value)) {
-            // 여기서 문자별로 산술식에 붙힐지 말지 코드를 추가해야 한다.
             if( value === 'delete') {
                 if (mathemeatical_expression.length == 0) {
 
@@ -41,20 +43,30 @@ function click_buttons() {
                     mathemeatical_expression = mathemeatical_expression.substring(0, mathemeatical_expression.length - 1);
                 }
 
-            } else if (['÷', 'x', '-', '+', '.'].includes(value)) {
-                char_last = mathemeatical_expression.substring(mathemeatical_expression.length - 1, mathemeatical_expression.length);
-
+            } else if (['÷', 'x', '-', '+'].includes(value)) {
                 if (isNaN(char_last)) {
-                    if (value === '.' && char_last !== '.') {
 
-                        value = '0' + value;
-
-                        mathemeatical_expression += value;
-                    }
                 } else {
                     mathemeatical_expression += value;
                 }
+            } else if (value === '.') {
+                // 연산자를 기준으로 숫자를 나눠서 이 값에 .이 들어 있으면 .이 또 들어는 것을 제한하는 코드.
+                const regex_operator = /[÷x\-+]/;
+                const expression_list = mathemeatical_expression.split(regex_operator);
+                const len_expression_list = expression_list.length;
 
+                if (expression_list[len_expression_list - 1].includes('.')) {
+                    value = '';
+                }
+
+                if (['÷', 'x', '-', '+'].includes(char_last) || mathemeatical_expression.length == 0) {
+                    value = '0' + value;
+                    mathemeatical_expression += value;
+                } else if (isNaN(char_last)) {
+
+                } else {
+                    mathemeatical_expression += value;
+                }
             }
         } else {
             mathemeatical_expression += value;
