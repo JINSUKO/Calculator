275919968f2d20658436d0439bf7f405b75302d8
고진수 <rhwkwk789@gmail.com>
2024-07-04 18:47:51 +0900

    feature: add calculation
    complete  calculation logics of 4 basic operator
    
    return evaluation of mathematical expresstion

--- Calculator_web/src/main/java/com/practice/calculator_web/controller/CalculationController.java
+++ Calculator_web/src/main/java/com/practice/calculator_web/controller/CalculationController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.Stack;
 
 @RestController
@@ -17,13 +16,13 @@ public class CalculationController {
 
     @PostMapping("/standard")
     public String getStandardOutput(@RequestBody String mathematicalExpression) {
-        evaluateExpression(splitExpression(mathematicalExpression));
-        return "Standard Output";
+
+        return evaluateExpression(splitExpression(mathematicalExpression));
     }
 
-    private static Queue<String> splitExpression(String expression) {
+    private static Deque<String> splitExpression(String expression) {
 
-        Queue<String> oper_queue = new LinkedList<>();
+        Deque<String> oper_queue = new LinkedList<>();
 
         String[] operandList = expression.split("[÷x\\-+]");
 
@@ -33,11 +32,11 @@ public class CalculationController {
             if (i == 0) {
                 if (operandList[i].isEmpty()) {
                     i++;
-                    oper_queue.add("-" + operandList[i]);
+                    oper_queue.addLast("-" + operandList[i]);
                     tmp += "-" + operandList[i];
                     continue;
                 } else {
-                    oper_queue.add(operandList[i]);
+                    oper_queue.addLast(operandList[i]);
                     tmp += operandList[i];
                     continue;
                 }
@@ -45,50 +44,43 @@ public class CalculationController {
 
             String operator = Character.toString(expression.charAt(tmp.length()));
             tmp += operator;
-            oper_queue.add(operator);
+            oper_queue.addLast(operator);
 
             tmp += operandList[i];
 
-            oper_queue.add(operandList[i]);
+            oper_queue.addLast(operandList[i]);
 
         }
 
         return oper_queue;
     }
 
-    private static void evaluateExpression(Queue<String> queue) {
-        System.out.println(queue);
+    private static String evaluateExpression(Deque<String> queue) {
 
         Stack<String> stack = new Stack<>();
 
-        Queue<String> operatorMultiandDiv = getMultiandDiv(queue);
+        Deque<String> operatorMultiandDiv = getMultiandDiv(queue);
 
-        String tmp;
+        String tmp = null;
 
-        while (queue.size() != 0 || stack.size() != 1) {
+        while (!(queue.size() == 0 && stack.size() == 1)) {
+
             tmp = queue.poll();
 
             if (isOperator(tmp)) {
-                // x ÷ 연산자 있으면 계산
-                if (operatorMultiandDiv.size() != 0) {
 
-                // x ÷ 연산자 없으면 - + 연산자 계산
-                } else {
-
-                }
-
+                computeWithCollections(queue, stack, tmp, operatorMultiandDiv);
 
             } else {
-                 stack.push(tmp);
+                stack.push(tmp);
             }
-
         }
 
+        return stack.pop();
     }
 
     private static boolean isOperator(String string) {
         char ch = string.charAt(0);
-        System.out.println(ch);
 
          switch (ch) {
             case '-':
@@ -104,19 +96,50 @@ public class CalculationController {
         }
     }
 
-    private static Queue<String> getMultiandDiv(Queue<String> queue) {
+    private static Deque<String> getMultiandDiv(Deque<String> queue) {
 
-        Queue<String> queueOpers = new LinkedList<>();
+        Deque<String> queueOpers = new LinkedList<>();
 
         queue.forEach(num -> {
-            if (num.equals('÷')) {
-                queueOpers.add(num);
-            } else if (num.equals('x')) {
-                queueOpers.add(num);
+            if (num.equals("÷")) {
+                queueOpers.addLast(num);
+            } else if (num.equals("x")) {
+                queueOpers.addLast(num);
             }
         });
 
         return queueOpers;
+    }
+
+    private static void computeWithCollections(Deque<String> queue, Stack<String> stack, String operator, Deque<String> queueOpers) {
+        String result;
+
+        if (queueOpers.size() != 0 && (operator.equals("÷") || operator.equals("x"))) {
+
+            result = compute(stack.pop(), queue.poll(), operator);
+            stack.push(result);
+
+            queueOpers.remove();
+
+            // x ÷ 연산 후에 queue 가 비워진다.
+            // + - 연산을 추가로 하기 위해서 queue 에 값을 다시 넣는다.
+            if (queueOpers.size() == 0) {
+                // x ÷ 모든 연산 후에 다시 + - 연산을 시작하면서 여기에 다시 걸린다.
+                // 조건을 추가해야한다.
+                while (stack.size() != 0) {
+                    queue.addFirst(stack.pop());
+                }
+
+            }
+        } else if (queueOpers.size() == 0 && (operator.equals("-") || operator.equals("+"))) {
+
+            result = compute(stack.pop(), queue.poll(), operator);
+            stack.push(result);
+
+        // 계산이 안됐을 때, 연산자를 스택에 넣음.
+        } else {
+            stack.push(operator);
+        }
     }
 
     private static String compute(String operand1, String operand2, String opertor) {
